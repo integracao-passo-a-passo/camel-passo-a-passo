3c475deb7bf1a8908a83e0cb2cfca585ae1aa149
Otavio R. Piske <angusyoung@gmail.com>
2021-09-12 14:45:36 +0200

    Atualizado para o Camel Quarkus 2.2

--- kafka-camel-quarkus/src/test/java/kafka/app/quarkus/NativeKafkaResourceIT.java
+++
@@ -1,9 +0,0 @@
-package kafka.app.quarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeKafkaResourceIT extends KafkaResourceTest {
-
-    // Execute the same tests but in native mode.
-}
